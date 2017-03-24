d4b838fa98de9e36fda6ce9eab3d409fae25c6b5
jamesbews <jbews11@gmail.com>
2017-03-24 15:16:24 -0600

    Errors present

---
+++ app/src/main/java/uofc/bewsschmidtensf409finalproject/CalculatorGUI.java
@@ -0,0 +1,13 @@
+package uofc.bewsschmidtensf409finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorGUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_gui);
+    }
+}
